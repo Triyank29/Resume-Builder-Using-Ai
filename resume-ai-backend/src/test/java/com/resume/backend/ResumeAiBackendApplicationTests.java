820325690cf7542fc143ccd09fb8b55a8db82e79
Triyank29 <dhdhrhhrh321@gmail.com>
2025-03-01 01:42:42 +0530

    add this project

---
+++ resume-ai-backend/src/test/java/com/resume/backend/ResumeAiBackendApplicationTests.java
@@ -0,0 +1,21 @@
+package com.resume.backend;
+
+import com.resume.backend.service.ResumeService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+class ResumeAiBackendApplicationTests {
+
+	@Autowired
+	private ResumeService resumeService;
+
+	@Test
+	void contextLoads() throws IOException {
+		resumeService.generateResumeResponse("I am Triyank Bavaliya with 5 year of java experience and currently looking for job");
+	}
+
+}
